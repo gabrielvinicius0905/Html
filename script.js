47ea2f1aa711158281e9a7570f0c273ef1c41119
gabrielvinicius0905 <gabriel.felix.silva09@escola.pr.gov.br>
2025-04-15 08:06:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function hora(){
-    //relogio recebe a data atual
     let relogio = document.getElementById('relogio')
 
     let tempo = new Date();
-    let horas = tempo.getHours();
-    let minutos = tempo.getMinutes();
-    let segundos = tempo.getSeconds();
+    let horas = String(tempo.getHours()).padStart(2,'0');
+    let minutos = String(tempo.getMinutes()).padStart(2,'0');
+    let segundos = String(tempo.getSeconds()).padStart(2,'0');
     
     relogio.innerHTML = `${horas}:${minutos}:${segundos}`
 
 
-
-
-
+    
   }
   
-  hora();
+  setInterval(hora,1000)
